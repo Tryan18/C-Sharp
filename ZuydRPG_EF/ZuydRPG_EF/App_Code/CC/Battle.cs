19d8c0493cd4430834b79133cafd86cce4e52d7d
Tryan18 <Tryan18@gmail.com>
2015-06-08 15:09:57 +0200

    ZuydRPG_EF Initial Version

---
+++ ZuydRPG_EF/ZuydRPG_EF/App_Code/CC/Battle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BU;
+
+namespace CC
+{
+    /// <summary>
+    /// Summary description for CCBattle
+    /// </summary>
+    public class Battle
+    {
+        public Battle()
+        {
+            
+        }
+    }
+}
