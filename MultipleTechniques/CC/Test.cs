588dbd91033e090c9499fc47e00c23ad6c8c708d
Tryan18 <Tryan18@gmail.com>
2016-05-13 14:49:15 +0200

    Multiple Techniques
    Shows Multiple Techniques in C#
    -Threading+mutex
    -Custom eventhandlers
    -Reflection example

---
+++ MultipleTechniques/CC/Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CC
+{
+    public class Test
+    {
+        private int getal = 0;
+        //Custom Event Handler
+        public event Update UpdateHandler = null;
+        //Custom Delegate
+        public delegate void Update(int getal);
+
+        public Test()
+        {
+
+        }
+
+        public void Modify()
+        {
+            getal++;
+            if(getal % 10 == 0)
+            {
+               if(UpdateHandler != null)
+                {
+                    UpdateHandler(getal);
+                }
+            }
+        }
+
+        public int Modify(int dummy)
+        {
+            return 0;
+        }
+    }
+}
